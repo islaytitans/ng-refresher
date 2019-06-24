2ddc8dce0ca409d949fdbde94aacd14f037a4721
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2019-06-24 18:00:10 +0100

    http get example

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -17,10 +17,10 @@ export class PersonsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.personList = this.personsService.persons;
     this.personListSubscription = this.personsService.personsChanged.subscribe(persons => {
       this.personList = persons;
     });
+    this.personsService.fetchPersons();
   }
 
   ngOnDestroy(): void {
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
-
 export class PersonsService {
   personsChanged = new Subject<string[]>();
-  persons: string[] = ['Max', 'Manuel', 'Anna'];
+  persons: string[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  fetchPersons() {
+    this.http.get<any>('https://swapi.co/api/people')
+    .pipe(map(responseData => {
+      return responseData.results.map(character => character.name);
+    })
+    )
+    .subscribe(transformedData => {
+      this.personsChanged.next(transformedData);
+    });
+  }
 
   addPerson(name: string) {
     this.persons.push(name);
